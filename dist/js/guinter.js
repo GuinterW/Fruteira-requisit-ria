405cf9ab98941ac36c8df774ad7ead944dea591e
Bruno Agostini Marques <bruno.marques@digitaldesk.com.br>
2016-05-18 14:21:44 -0300

    Organização layout

--- dist/js/guinter.js
+++ dist/js/guinter.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
-	$("#CaixaValor").keyup(function(){
-        var numero = $(this);
-        numero.val(NumerosVirgulasPontos(/[^0-9,.]+/g,'', numero.val()));
-    });
-    $("#CaixaEstoque").keypress(SomenteNumeros);
-	$("#TabelaAdicionar").hide();
+	$("#CaixaValor").keypress(NumerosPontosVirgulas);
+    $("#CaixaEstoque").keypress(NumerosPontosVirgulas);
+    $("#CampoPesquisa").keypress(SomenteNumeros);
 	$('#Adicionar').show();
 	$("#CaixaValor").maskMoney({showSymbol:true, symbol:"", decimal:".", thousands:","});
+	inicio();
 	getJSON();
 });
 
@@ -14,20 +12,18 @@ $(document).ready(function(){
 var	url="http://localhost:3000/product/";
 var mensagens = {
 	invalidez:"OPS! Esta opção é inválida! Escolha outra, por favor!",
-	deleteSucesso: "A opção foi apagada com sucesso!",
-	adionarSucesso: "A opção foi adicionada com sucesso!",
 	optionNull: "Selecione uma alternativa...",
 	campoNull: "Preencha corretamente os campos!",
 	confirma: 'Você está certo disto?'
 }
 
-//função que aceita números,vírgulas e pontos.
-function NumerosVirgulasPontos( texto, emBranco, campo ){
-	return campo.replace( texto, emBranco );
+function inicio (){
+	esconde(['#CampoPesquisa','#CampoSelecionar','#TabelaAdicionar',]);
+	mostra(['#BotaoAdicionar','#BotaoPesquisar','#BotaoSelecionar']);
 }
 
-//função que aceita somente números.
-function SomenteNumeros(e){
+//função que aceita decimais.
+function NumerosPontosVirgulas(e){
 	var texto = $("#CaixaEstoque").val();
 	var indexvir = texto.indexOf(",");
    	var indexpon = texto.indexOf(".");
@@ -39,6 +35,13 @@ function SomenteNumeros(e){
    	if (e.which == 46) { 
    		if (indexvir !== -1 || indexpon !== -1) return false; 
    	}
+}
+
+//Apenas números inteiros
+function SomenteNumeros (e){
+	var texto = $("#CaixaEstoque").val();
+	if ( e.which == 8 || e.which == 0 ) return true;
+   	if ( e.which < 48 || e.which > 57 ) return false;
 }
 
 //função que chama os itens da url.
@@ -64,20 +67,25 @@ function Cliques(database){
 		BotaoDeletar();
 	});
 	$("#BotaoAdicionar").click(function(){
-		$("#TabelaAdicionar").show();
-		$('#Tudo').html('').hide();
-		$(this).hide();
-		$('#Enviar').hide();
-		esconde(['#BotaoEditar', '#BotaoDeletar', '#BotaoAdicionar', '#Enviar']);
-		$('#Adicionar').show();
+		$('#Tudo').html('');
+		mostra(['#TabelaAdicionar','#Adicionar']);
+		esconde(['#CampoSelecionar','#CampoPesquisa','#Tudo']);
 		BotaoAdicionar();
 	});
 	$("#BotaoEditar").click(function(){
-		$("#TabelaAdicionar").show();
-		$('#Tudo').html('').hide();
-		esconde(['#BotaoEditar', '#BotaoDeletar', '#BotaoAdicionar', '#Adicionar']);
-		$('#Enviar').show();
+		$('#Tudo').html('');
+		mostra(['#TabelaAdicionar','#Enviar']);
+		esconde(['#CampoSelecionar', '#Adicionar','#Tudo']);
 		BotaoEditar();
+	});
+	$("#BotaoPesquisar").click(function(){
+		esconde(['#TabelaAdicionar','#CampoSelecionar','#Tudo'])
+		mostra(['#CampoPesquisa','#BotaoExibir']);
+		//$("#CampoPesquisa").show();
+	});
+	$("#BotaoSelecionar").click(function(){
+		esconde(['#TabelaAdicionar','#CampoPesquisa'])
+		$("#CampoSelecionar").show();
 	});
 }
 
@@ -98,7 +106,7 @@ function TesteVar(z){
 	var z = $('#Select').val();
 	if (z>0){
 		$.getJSON([url] + z, function(database){
-			mostra(['#BotaoEditar', '#BotaoDeletar', '#BotaoAdicionar']);
+			mostra(['#BotaoEditar', '#BotaoDeletar']);
 			Escritas(database);
 		})
 	}
@@ -122,10 +130,10 @@ function Escritas(database){
 //função que apaga a opção quando o botão 'Deletar' for pressionado.
 function BotaoDeletar(){
 	var z = $('#Select').val();
-	if(confirm(mensagens.confirma)) ajax(url + z, 'DELETE', mensagens.deleteSucesso, '');
+	if(confirm(mensagens.confirma)) ajax(url + z, 'DELETE', '');
 }
 
-function ajax(url, type, mensagem, data){
+function ajax(url, type, data){
 	$.ajax({
 		url: url,
 		type: type,
@@ -134,9 +142,9 @@ function ajax(url, type, mensagem, data){
 			$("#TabelaAdicionar").hide();
 			$('#Tudo').html('').hide();
 			getJSON();
-			alert(mensagem);
 		}
-	})
+	});
+	esconde(['#TabelaAdicionar','#CampoPesquisa','#CampoSelecionar'])
 }
 
 //função que adiciona uma opção quando o botão 'Adicionar' for pressionado.
@@ -149,9 +157,9 @@ function BotaoAdicionar(){
 		var data = {nome: Produto,valor: Valor,status: Status,estoque: Estoque};
 		if($("#CaixaProduto").val()!=='' && $("#CaixaValor").val()!=='' && $("#CaixaEstoque").val()!==''){
 			if(confirm(mensagens.confirma)){
-				$('#Adicionar').hide();
-				$('#BotaoAdicionar').show();
-				ajax(url, 'POST', mensagens.adionarSucesso, data);
+				//$('#Adicionar').hide();
+				//$('#BotaoAdicionar').show();
+				ajax(url, 'POST', data);
 			}
 		}
 		else alert(mensagens.campoNull);
@@ -170,7 +178,7 @@ function BotaoEditar(){
 			if(confirm(mensagens.confirma)){	
 				$('#Enviar').hide();
 				$('#BotaoAdicionar').show();
-				ajax(url + z, 'PUT', mensagens.adionarSucesso, data);
+				ajax(url + z, 'PUT', data);
 			}
 		}
 		else alert(mensagens.campoNull);

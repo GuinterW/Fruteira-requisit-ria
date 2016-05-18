5013b390f3b07cabcb9a78407b5e92a5e88ee6dd
Bruno Agostini Marques <bruno.marques@digitaldesk.com.br>
2016-05-18 10:05:28 -0300

    Botões desabilitados #9

--- dist/js/guinter.js
+++ dist/js/guinter.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     });
     $("#CaixaEstoque").keypress(SomenteNumeros);
 	$("#TabelaAdicionar").hide();
+	$('#Adicionar').show();
 	$("#CaixaValor").maskMoney({showSymbol:true, symbol:"", decimal:".", thousands:","});
 	getJSON();
 });
@@ -20,108 +21,138 @@ var mensagens = {
 }
 
 //função que aceita números,vírgulas e pontos.
-	function NumerosVirgulasPontos( texto, emBranco, campo ){
-		return campo.replace( texto, emBranco );
-	}
-//função que aceita somente números.
-	function SomenteNumeros(e){
-		var texto = $("#CaixaEstoque").val();
-		var indexvir = texto.indexOf(",");
-     	var indexpon = texto.indexOf(".");
-    	if ( e.which == 8 || e.which == 0 ) return true;
-    	if ( e.which != 44 && e.which != 46 && e.which < 48 || e.which > 57 ) return false;
-    	if (e.which == 44) { 
-    		if (indexvir !== -1 || indexpon !== -1) return false; 
-    	}
-    	if (e.which == 46) { 
-    		if (indexvir !== -1 || indexpon !== -1) return false; 
-    	}
-	}
-//função que chama os itens da url.
-	function getJSON(){
-		$.getJSON([url], function(database){
-			var alternativas='<option value="#">'+mensagens.optionNull+'</option>';
-			for (var g=0; g<database.length; g++){
-				alternativas+='<option value='+database[g].id + '>' + database[g].nome + '</option>';
-			}
-			$("#Select").html(alternativas);
-			Cliques(database);	
-		});
-	}
-//função dos botões.
-	function Cliques(database){
-		$("#BotaoExibir").click(function(){
-			TesteVar();
-		});
-		$("#BotaoDeletar").click(function(){
-			BotaoDeletar();
-		});
-		$("#BotaoAdicionar").click(function(){
-			$("#TabelaAdicionar").show();
-			BotaoAdicionar(database);
-		});
-		$("#BotaoEditar").click(function(){
-			$("#TabelaAdicionar").show();
+function NumerosVirgulasPontos( texto, emBranco, campo ){
+	return campo.replace( texto, emBranco );
+}
 
-		});
+//função que aceita somente números.
+function SomenteNumeros(e){
+	var texto = $("#CaixaEstoque").val();
+	var indexvir = texto.indexOf(",");
+   	var indexpon = texto.indexOf(".");
+   	if ( e.which == 8 || e.which == 0 ) return true;
+   	if ( e.which != 44 && e.which != 46 && e.which < 48 || e.which > 57 ) return false;
+   	if (e.which == 44) { 
+   		if (indexvir !== -1 || indexpon !== -1) return false; 
+   	}
+   	if (e.which == 46) { 
+   		if (indexvir !== -1 || indexpon !== -1) return false; 
+   	}
+}
+
+//função que chama os itens da url.
+function getJSON(){
+	$.getJSON([url], function(database){
+		var alternativas='<option value="#">'+mensagens.optionNull+'</option>';
+		for (var g=0; g<database.length; g++){
+			alternativas+='<option value='+database[g].id + '>' + database[g].nome + '</option>';
+		}
+		$("#Select").html(alternativas);
+		Cliques(database);	
+	});
+}
+
+//função dos botões.
+function Cliques(database){
+	$("#BotaoExibir").click(function(){
+		mostra(['#BotaoEditar', '#BotaoDeletar', '#BotaoAdicionar']);
+		TesteVar();
+	});
+	$("#BotaoDeletar").click(function(){
+		esconde(['#BotaoEditar', '#BotaoDeletar']);
+		mostra(['#BotaoAdicionar']);
+		BotaoDeletar();
+	});
+	$("#BotaoAdicionar").click(function(){
+		$("#TabelaAdicionar").show();
+		$('#Tudo').html('').hide();
+		$(this).hide();
+		$('#Enviar').hide();
+		esconde(['#BotaoEditar', '#BotaoDeletar', '#BotaoAdicionar']);
+		$('#Adicionar').show();
+		BotaoAdicionar(database);
+	});
+	$("#BotaoEditar").click(function(){
+		$("#TabelaAdicionar").show();
+		esconde(['#BotaoEditar', '#BotaoDeletar', '#BotaoAdicionar']);
+		$('#Enviar').show();
+	});
+}
+
+function esconde(itens){
+	for(var x=0; x<itens.length; x++){
+		$(itens[x]).stop().hide();
 	}
+}
+
+function mostra(itens){
+	for(var x=0; x<itens.length; x++){
+		$(itens[x]).stop().show();
+	}
+}
+
 //função que testa a variável 'z' para que a primeira opção não seja escolhida.
-	function TesteVar(z){
-		var z = $('#Select').val();
-		if (z>0){
-			$.getJSON([url] + z, function(database){
-				Escritas(database);
+function TesteVar(z){
+	var z = $('#Select').val();
+	if (z>0){
+		$.getJSON([url] + z, function(database){
+			Escritas(database);
+		})
+	}
+	else{
+		alert(mensagens.invalidez);
+	}
+}
+
+//função que faz aparecer o ítem solicitado.
+function Escritas(database){
+	$("#TabelaAdicionar").hide();
+	$("#Tudo").show();
+	var respostas='';
+	respostas+= '<b>Produto: </b>' + database.nome + '<br>';
+	respostas+= '<b>Valor:</b> R$ ' + database.valor + '<br>';
+	respostas+= '<b>Status: </b>' + database.status + '<br>';
+	respostas+= '<b>Estoque: </b>' + database.estoque;
+	$('#Tudo').html(respostas);
+}
+
+//função que apaga a opção quando o botão 'Deletar' for pressionado.
+function BotaoDeletar(){
+	var z = $('#Select').val();
+	TesteVar();
+	$.ajax({
+		url: [url] + z,
+		type: "DELETE",
+		success: function(){
+			$("#TabelaAdicionar").hide();
+			getJSON();
+			$('#Tudo').html('').hide();
+			alert(mensagens.deleteSucesso);
+		}
+	})
+}
+
+//função que adiciona uma opção quando o botão 'Adicionar' for pressionado.
+function BotaoAdicionar(database){
+	$("#Adicionar").click(function(database){
+		var Produto = $('#CaixaProduto').val();
+		var Valor = $('#CaixaValor').val();
+		var Status = $('#CaixaStatus').val();
+		var Estoque = $('#CaixaEstoque').val();
+		if($("#CaixaProduto").val()!=='' && $("#CaixaValor").val()!=='' && $("#CaixaEstoque").val()!==''){
+			$.ajax({
+				url: [url],
+				type: "POST",
+				data: {nome: Produto,valor: Valor,status: Status,estoque: Estoque,}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ,
+				success: function(){
+					$("#TabelaAdicionar").hide();
+					getJSON();
+					alert(mensagens.adionarSucesso);
+				}
 			})
 		}
-		else{
-			alert(mensagens.invalidez);
-		}
-	}
-//função que faz aparecer o ítem solicitado.
-	function Escritas(database){
-			$("#TabelaAdicionar").hide();
-			$("#Tudo").show();
-			var respostas='';
-			respostas+= '<b>Produto: </b>' + database.nome + '<br>';
-			respostas+= '<b>Valor:</b> R$ ' + database.valor + '<br>';
-			respostas+= '<b>Status: </b>' + database.status + '<br>';
-			respostas+= '<b>Estoque: </b>' + database.estoque;
-			$('#Tudo').html(respostas);
-	}
-//função que apaga a opção quando o botão 'Deletar' for pressionado.
-	function BotaoDeletar(){
-		var z = $('#Select').val();
-		TesteVar();
-		$.ajax({
-			url: [url] + z,
-			type: "DELETE",
-			success: function(){
-				$("#TabelaAdicionar").hide();
-				getJSON();
-				alert(mensagens.deleteSucesso);
-				$('#Tudo').html('').hide();
-			}
-		})
-	}
-//função que adiciona uma opção quando o botão 'Adicionar' for pressionado.
-	function BotaoAdicionar(database){
-		$("#Adicionar").click(function(database){
-			var Produto = $('#CaixaProduto').val();
-			var Valor = $('#CaixaValor').val();
-			var Status = $('#CaixaStatus').val();
-			var Estoque = $('#CaixaEstoque').val();
-			if($("#CaixaProduto").val()!=='' && $("#CaixaValor").val()!=='' && $("#CaixaEstoque").val()!==''){
-				$.ajax({
-					url: [url],
-					type: "POST",
-					data: {nome: Produto,valor: Valor,status: Status,estoque: Estoque,}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ,
-					success: function(){
-						$("#TabelaAdicionar").hide();
-						getJSON();
-						alert(mensagens.adionarSucesso);
-					}
-				})
-			}
-			else alert(mensagens.campoNull);
-		})
-	}
+		else alert(mensagens.campoNull);
+		$('#Adicionar').hide();
+		$('#BotaoAdicionar').show();
+	})
+}
